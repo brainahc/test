c8088e762e08af8516d863dc7776df406f18bf13
brainahc <35502938+brainahc@users.noreply.github.com>
2023-02-24 04:21:49 +0530

    Add files via upload

---
+++ rest_api.py
@@ -0,0 +1,13 @@
+import requests
+import json                 
+import csv
+
+
+base_url = 'https://reqres.in/api/users?page=2'
+resp = requests.get(base_url)
+print(resp.json())
+
+
+with open ('api3.csv', 'w') as optput_file:
+    writer = csv.writer(optput_file)
+    writer.writerow(resp)
